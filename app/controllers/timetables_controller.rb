47997971890718d9c5af7251fb3fe2193a6fed71
fukk.u.3kpvc <ykwnd@yahoo.com>
2019-04-01 13:34:15 +0500

    AJAX filter and search methods

--- app/controllers/timetables_controller.rb
+++ app/controllers/timetables_controller.rb
@@ -4,13 +4,24 @@ class TimetablesController < ApplicationController
   def show
     group = StudentGroup.find_by(name: params[:group])
     if group.blank?
-      flash[:error] = 'Группа не найдена'
-      redirect_to root_path
+      respond_to do |format|
+        format.html do
+          flash[:error] = 'Группа не найдена'
+          redirect_to root_path
+        end
+      end
       return
     end
-
     @records = group.timetable.timetable_records.preload(
         :subject, :lecturer, :room, :subject_time_period
     ).group_by(&:day_of_week)
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def filter_student_groups_by_course
+    @groups = StudentGroup.where(student_course: StudentCourse.find_by(name: params[:selected_course]))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   resources :timetables, only: %i[index]
   get '/search' => 'timetables#show', as: :search_timetable_by_group
+  get 'filter_student_groups_by_course' => 'timetables#filter_student_groups_by_course'
   root to: 'timetables#index'
 end
